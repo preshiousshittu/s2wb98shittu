37330d567ae9e8d277e68ecb327b0fb89a4718d8
preshiousshittu <54418932+preshiousshittu@users.noreply.github.com>
2022-04-06 14:35:27 -0500

    adds selector

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var booksRouter = require('./routes/books');
 var usersRouter = require('./routes/users');
 var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -25,6 +26,8 @@ app.use('/', indexRouter);
 app.use('/books', booksRouter);
 app.use('/users', usersRouter);
 app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
