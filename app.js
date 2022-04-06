4b07bac7f34961e8709c98b6f86cdfc3da6b64c4
preshiousshittu <54418932+preshiousshittu@users.noreply.github.com>
2022-04-06 13:18:02 -0500

    creates query

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var booksRouter = require('./routes/books');
 var usersRouter = require('./routes/users');
+var addmodsRouter = require('./routes/addmods');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/books', booksRouter);
 app.use('/users', usersRouter);
+app.use('/addmods', addmodsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
